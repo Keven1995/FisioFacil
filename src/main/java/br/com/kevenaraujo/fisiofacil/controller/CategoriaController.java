6529b8c512bf7c21933cd025231c7a0e5710b579
Keven <lkeven77@gmail.com>
2024-09-29 10:50:40 -0300

    Adicionado entidades de categoria e membros

---
+++ src/main/java/br/com/kevenaraujo/fisiofacil/controller/CategoriaController.java
@@ -0,0 +1,25 @@
+package br.com.kevenaraujo.fisiofacil.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.kevenaraujo.fisiofacil.entity.Categoria;
+import br.com.kevenaraujo.fisiofacil.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listarCategorias() {
+        return categoriaRepository.findAll();
+    }
+}
+
---
+++ src/main/java/br/com/kevenaraujo/fisiofacil/repository/MembroRepository.java
@@ -0,0 +1,11 @@
+package br.com.kevenaraujo.fisiofacil.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.kevenaraujo.fisiofacil.entity.Membro;
+
+@Repository
+public interface MembroRepository extends JpaRepository<Membro, Long> {
+}
+
