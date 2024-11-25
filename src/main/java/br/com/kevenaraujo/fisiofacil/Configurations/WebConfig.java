4330ca1f21e7e1aa029ee28838e435b92f85a12a
Keven Lucas Pereira Araujo <lkeven77@gmail.com>
2024-11-25 18:49:35 -0300

    Atualizando CORS

--- src/main/java/br/com/kevenaraujo/fisiofacil/Configurations/WebConfig.java
+++ src/main/java/br/com/kevenaraujo/fisiofacil/Configurations/WebConfig.java
@@ -11,9 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://fisio-facil-front-nas6pg7kr-keven1995s-projects.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
